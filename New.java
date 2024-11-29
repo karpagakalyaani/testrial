fbc9b4f0b0052d43c6683d8a3ccba4e58a3eca55
karpagakalyaani <113163768+karpagakalyaani@users.noreply.github.com>
2024-11-28 22:40:17 -0800

    Add files via upload

---
+++ New.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+import java.util.*;
+
+
+public class New {
+
+        public static void main(String[] args) throws Exception{
+        
+                						long t = 1600950146L;
+							Date f = new Date(t*1000L);
+							System.out.println(f.toLocaleString());
+
+        }
+}
+
